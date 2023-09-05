a9bcc16e3318b3378396dcede020e1bed6ef4dd3
azizollah <azizollahpayandeh1@gmail.com>
2023-09-05 20:12:28 +0430

    complete about components

--- src/Components/AboutMe/AbouMe.js
+++ src/Components/AboutMe/AbouMe.js
@@ -7,14 +7,14 @@ export default function AbouMe() {
   const from = "Iran, Mashhad";
 
   return (
-    <div className="w-[100%] h-[100vh] bg-[rgb(28, 30, 31)]  ">
+    <div className="w-[100%] xl:h-[100vh] 2xl:h-[80vh] bg-[rgb(28, 30, 31)]  ">
       <div className="flex-col justify-center items-center text-center flex ">
         <h1 className="text-3xl font-bold xl:mt-28 mt-24">Know Me More</h1>
         <div className="w-[70px]  bg-[#20c997] mt-2 h-[2px]" />
       </div>
 
       <div className=" grid xl:grid-cols-12 ">
-        <div className="xl:col-span-7   text-center xl:text-left ml-3 mr-3 xl:ml-14 2xl:ml-36 mt-12  ">
+        <div className="xl:col-span-7   text-center xl:text-left ml-3 mr-3 xl:ml-14 2xl:ml-36 mt-16  ">
           <h1 className="text-3xl font-semibold ">
             I'm <span className="text-[#20c997]">Azizollah Payandeh</span> a Web
             Developer
@@ -36,14 +36,44 @@ export default function AbouMe() {
 
         <div className="xl:col-span-5 xl:mt-9  xl:ml-14 2xl:ml-10 mt-12 ml-5">
           <h1 className=" mt-5 "><span className="font-semibold">Name:  </span> <span className="opacity-80">{name} </span></h1>
-          <hr  className="2xl:opacity-0 opacity-50 mt-2 "/>
-          <li className="list-none mt-5"> <a href="#" ><span className="font-semibold">Email:  </span> <span className="opacity-50 mt-2 text-[#20c997]">{email}</span></a></li>
-          <hr  className="2xl:opacity-0 opacity-50 mt-2 "/>
+          <hr  className="2xl:opacity-0 opacity-50 mt-2 xl:w-[400px]  "/>
+          <li className="list-none mt-5"> <a href="#" ><span className="font-semibold">Email:  </span> <span className="opacity-50 mt-2 xl:w-[400px]  text-[#20c997]">{email}</span></a></li>
+          <hr  className="2xl:opacity-0 opacity-50 mt-2 xl:w-[400px]  "/>
           <h1 className=" mt-5"><span className="font-semibold">Age:  </span> <span className="opacity-80">{age}</span> </h1>
-          <hr  className="2xl:opacity-0 opacity-50 mt-2 "/>
+          <hr  className="2xl:opacity-0 opacity-50 mt-2 xl:w-[400px]  "/>
           <h1 className=" mt-5"><span className="font-semibold">From:  </span> <span className="opacity-80">{from}</span> </h1>
           <button className=" 2xl:mt-7 mt-8 xl:mt-6 bg-green-400 hover:bg-green-300 w-[170px] h-[50px] rounded-3xl transition duration-300 ease-in-out">Download CV</button>
         </div>
+
+      </div>
+
+      <div className="flex xl:justify-evenly flex-wrap xl:mt-20 mt-12  ml-6 ">
+
+      <div  className="flex flex-col text-center h-[140px] w-[250px] xl:border-r-[1px]   justify-center  "> 
+          <h1 className="text-5xl opacity-50 ">2+</h1>
+          <p className="mt-2 opacity-85">Years Experiance</p>
+        </div>
+
+
+
+      <div  className="flex flex-col text-center h-[140px] w-[250px] xl:border-r-[1px]  justify-center  "> 
+          <h1 className="text-5xl opacity-50 ">50+</h1>
+          <p className="mt-2 opacity-85">Happy Clients</p>
+        </div>
+
+
+        <div  className="flex flex-col text-center h-[140px]16 w-[250px] xl:border-r-[1px]  justify-center  "> 
+          <h1 className="text-5xl opacity-50  mt-3">20+</h1>
+          <p className="mt-2 opacity-85">Projects Done</p>
+        </div>
+
+
+        <div  className="flex flex-col text-center h-[140px]16 w-[250px]  justify-center"> 
+          <h1 className="text-5xl opacity-50 mt-3 ">5+</h1>
+          <p className="mt-2 opacity-85">Get Awards</p>
+        </div>
+
+
       </div>
     </div>
   );
